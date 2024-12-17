2805ee19b3ef1dcc9cead47240f3a9e841aaec65
Kim-yerin0904 <yeelin0904@naver.com>
2024-12-17 11:51:24 +0900

    update form1,cs

--- example241217/Form1.cs
+++ example241217/Form1.cs
@@ -19,26 +19,18 @@ namespace example241217
             InitializeComponent();
 
             string[] result = new string[10];
-            string example = "동해 물과 백두산이";
-            result[0] = example.IndexOf("백두산").ToString();
-            example = "토요일에 먹는 토마토";
-            result[1] = example.LastIndexOf("토").ToString();
-            example = "질서 있는 퇴장";
-            result[2] = example.Contains("퇴").ToString();
-            example = "그 사람의 그림자는 그랬다.";
-            result[3] = example.Replace("그","이");
-            example = "삼성 갤럭시";
-            result[4] = example.Insert(2, "애플");
-            example = "오늘은 왠지 더 배고프다";
-            result[5] = example.Remove(7,1);
-            example = "이름,나이,전화번호";
-            string[] words = example.Split(',');
+            result[0] = "동해 물과 백두산이".IndexOf("백두산").ToString();
+            result[1] = "토요일에 먹는 토마토".LastIndexOf("토").ToString();
+            result[2] = "질서 있는 퇴장".Contains("퇴").ToString();
+            result[3] = "그 사람의 그림자는 그랬다.".Replace("그","이");
+            result[4] = "삼성 갤럭시".Insert(2, "애플");
+            result[5] = "오늘은 왠지 더 배고프다".Remove(7,1);
+            string[] words = "이름,나이,전화번호".Split(',');
             for (int i=0; i<words.Length; i++)
             {
                 result[i+6] = words[i];
             }
-            example = "우리 나라 만세";
-            result[9] = example.Substring(3,2);
+            result[9] = "우리 나라 만세".Substring(3,2);
 
             foreach(string a in result)
             {
